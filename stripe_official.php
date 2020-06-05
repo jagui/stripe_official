0be06188143e2c07fb07430cb843ad0cedcbd49f
Juan Agüí <juan@juanagui.com>
2020-06-05 18:18:50 +0200

    version 2.1.0 release files

--- stripe_official.php
+++ stripe_official.php
@@ -226,7 +226,7 @@ class Stripe_official extends PaymentModule
     {
         $this->name = 'stripe_official';
         $this->tab = 'payments_gateways';
-        $this->version = '@version@';
+        $this->version = '2.1.0';
         $this->author = '202 ecommerce';
         $this->bootstrap = true;
         $this->display = 'view';
